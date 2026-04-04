efec0a9d97c5f9b3fd1198b56564e3936489ae86
devinci-it <vince.dev@icloud.com>
2026-04-04 15:59:03 -0700

    feat(core): add constants definition

---
+++ src/Constants.php
@@ -0,0 +1,13 @@
+<?php
+namespace DevinciIT\Modulr;
+
+class Constants
+{
+    public const ASSET_BASE = '/vendor/modulr/';
+    public const DEFAULT_VIEW = 'view.php';
+    public const DEFAULT_CSS = 'style.css';
+    public const DEFAULT_JS = 'script.js';
+    public const CLASS_PREFIX = 'modulr-';
+    public const DATA_COMPONENT_ATTR = 'data-component';
+    public const ID_PREFIX = 'modulr-';
+}
